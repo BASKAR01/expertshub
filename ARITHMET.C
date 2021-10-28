4e09846f723e451ef30bcfb785952ddad35dcc23
BASKAR01 <93314128+BASKAR01@users.noreply.github.com>
2021-10-28 14:05:12 +0530

    Add files via upload

---
+++ ARITHMET.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int no1,no2,o1,o2,o3,o4,o5;
+  clrscr();
+  printf("Enter input values:");
+  scanf("%d""%d",&no1,&no2);
+  //printf("Enter input 2:");
+  //scanf("%d",&no2);
+  o1=no1+no2;
+  printf("\nsum of the addition are%d",o1);
+  o2=no1-no2;
+  printf("\nsum of the subtraction are:%d",o2);
+  o3=no1*no2;
+  printf("\nsum of the multiplication are :%d",o3);
+  o4=no1/no2;
+  printf("\nsum of the division are:%d",o4);
+  o5=no1%no2;
+  printf("\nsum of the modulus are:%d",o5);
+  getch();
+  }
+
+
+
